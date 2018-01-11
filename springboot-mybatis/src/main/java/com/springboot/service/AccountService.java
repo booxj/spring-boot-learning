938af007a4fc7314481de277cee0e770f894fcde
汪博 <wangbo@soyea.com.cn>
2018-01-11 11:00:04 +0800

    springboot-mybatis

---
+++ springboot-mybatis/src/main/java/com/springboot/service/AccountService.java
@@ -0,0 +1,46 @@
+package com.springboot.service;
+
+import com.springboot.mapper.AccountMapper;
+import com.springboot.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: wb
+ * @data: 2018/1/11 10:53
+ * @see:
+ * @since:
+ */
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    public int add(String name, double money) {
+        return accountMapper.add(name, money);
+    }
+
+    public int update(String name, double money, int id) {
+        return accountMapper.update(name, money, id);
+    }
+
+    public int delete(int id) {
+        return accountMapper.delete(id);
+    }
+
+    public Account findAccount(int id) {
+        return accountMapper.findAccount(id);
+    }
+
+    public List<Account> findAccountList() {
+        return accountMapper.findAccountList();
+    }
+
+    public int updateMoney(double money, int id) {
+        return accountMapper.updateMoney(money, id);
+    }
+}
---
+++ springboot-mybatis/src/main/java/com/springboot/web/AccountController.java
@@ -0,0 +1,69 @@
+package com.springboot.web;
+
+import com.springboot.model.Account;
+import com.springboot.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: wb
+ * @data: 2018/1/11 10:55
+ * @see:
+ * @since:
+ */
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping("list")
+    public List<Account> getAccounts() {
+        return accountService.findAccountList();
+    }
+
+    @GetMapping("{id}")
+    public Account getAccountById(@PathVariable("id") int id) {
+        return accountService.findAccount(id);
+    }
+
+    @PutMapping("{id}")
+    public String updateAccount(@PathVariable("id") int id, @RequestParam(value = "name", required = true) String name,
+                                @RequestParam(value = "money", required = true) double money) {
+        int t = accountService.update(name, money, id);
+        if (t == 1) {
+            return "success";
+        } else {
+            return "fail";
+        }
+
+    }
+
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable(value = "id") int id) {
+        int t = accountService.delete(id);
+        if (t == 1) {
+            return "success";
+        } else {
+            return "fail";
+        }
+
+    }
+
+    @PostMapping("")
+    public String postAccount(@RequestParam(value = "name") String name,
+                              @RequestParam(value = "money") double money) {
+
+        int t = accountService.add(name, money);
+        if (t == 1) {
+            return "success";
+        } else {
+            return "fail";
+        }
+
+    }
+}
