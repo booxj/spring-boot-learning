44fc0d67a5879d3a70f05761d237ad21d724a651
汪博 <wangbo@soyea.com.cn>
2018-01-12 09:25:36 +0800

    springboot-jms

---
+++ springboot-jms/src/main/java/com/springboot/JmsApplication.java
@@ -0,0 +1,19 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: wb
+ * @data: 2018/1/11 18:27
+ * @see:
+ * @since:
+ */
+@SpringBootApplication
+public class JmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsApplication.class, args);
+    }
+}
