f8e31562c75e8441cd1a09216a5bf5a15cd5e597
booxj <1416890997@qq.com>
2018-07-25 16:53:08 +0800

    springboot-dubbo

--- springboot-dubbo/springboot-dubbo-consumer/src/main/java/com/springboot/controller/DubboController.java
+++ springboot-dubbo/springboot-dubbo-consumer/src/main/java/com/springboot/controller/DubboController.java
@@ -3,7 +3,6 @@ package com.springboot.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.springboot.model.User;
 import com.springboot.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ springboot-dubbo/springboot-dubbo-provider/src/main/java/com/springboot/DubboProviderApp.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+//@ImportResource({"classpath:dubbo-provider.xml"})
+public class DubboProviderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderApp.class, args);
+    }
+}
